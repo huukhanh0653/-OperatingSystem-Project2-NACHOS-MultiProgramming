1cc99edeef25b8f81591ba8820d85afd70a5d80e
= <=>
2023-05-20 22:25:13 +0700

    utils.h

--- nachos/NachOS-4.0/code/test/utils.h
+++ nachos/NachOS-4.0/code/test/utils.h
@@ -26,34 +26,34 @@ int len(char *str)
 
 int abs(int x) { return (x > 0 ? x : -x); }
 
-char* str(int num)
-{
-    int i = 0;
-    // Handle negative integers
-    if (num < 0) {
-        str[i++] = '-';
-        num = -num;
-    }
-    // Convert individual digits into a string
-    if (num == 0) {
-        str[i++] = '0';
-    } else {
-        while (num != 0) {
-            int rem = num % 10;
-            str[i++] = (rem > 9) ? (rem-10)+'a' : rem+'0';
-            num /= 10;
-        }
-    }
-    str[i] = '\0'; // Add null character to the end of the string
-    // Reverse the string
-    int len = i;
-    for (i = 0; i < len / 2; i++) {
-        char temp = str[i];
-        str[i] = str[len-i-1];
-        str[len-i-1] = temp;
-    }
-    return str;
-}
+// char* str(int num)
+// {
+//     int i = 0;
+//     // Handle negative integers
+//     if (num < 0) {
+//         str[i++] = '-';
+//         num = -num;
+//     }
+//     // Convert individual digits into a string
+//     if (num == 0) {
+//         str[i++] = '0';
+//     } else {
+//         while (num != 0) {
+//             int rem = num % 10;
+//             str[i++] = (rem > 9) ? (rem-10)+'a' : rem+'0';
+//             num /= 10;
+//         }
+//     }
+//     str[i] = '\0'; // Add null character to the end of the string
+//     // Reverse the string
+//     int len = i;
+//     for (i = 0; i < len / 2; i++) {
+//         char temp = str[i];
+//         str[i] = str[len-i-1];
+//         str[len-i-1] = temp;
+//     }
+//     return str;
+// }
 
 
 char *_concat(char s1[], char s2[])
